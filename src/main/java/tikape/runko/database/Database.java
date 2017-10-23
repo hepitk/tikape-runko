cde0e709b6b65b835c697a10abc2a0bfe725bbef
hepitk <31772375+hepitk@users.noreply.github.com>
2017-10-24 00:37:36 +0300

    2

--- src/main/java/tikape/runko/database/Database.java
+++ src/main/java/tikape/runko/database/Database.java
@@ -39,10 +39,18 @@ public class Database {
         ArrayList<String> lista = new ArrayList<>();
 
         // tietokantataulujen luomiseen tarvittavat komennot suoritusjärjestyksessä
-        lista.add("CREATE TABLE Opiskelija (id integer PRIMARY KEY, nimi varchar(255));");
-        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Platon');");
-        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Aristoteles');");
-        lista.add("INSERT INTO Opiskelija (nimi) VALUES ('Homeros');");
+        lista.add("CREATE TABLE RaakaAine (id integer PRIMARY KEY, nimi varchar(255));");
+        lista.add("INSERT INTO RaakaAine (nimi) VALUES ('Makee');");
+        lista.add("INSERT INTO RaakaAine (nimi) VALUES ('Huono');");
+        lista.add("INSERT INTO RaakaAine (nimi) VALUES ('Hyoky');");
+
+        
+        
+        
+        /*lista.add("CREATE TABLE Annos (id integer PRIMARY KEY, nimi varchar(255));");
+        lista.add("INSERT INTO Annos (nimi) VALUES ('Makee');");
+        lista.add("INSERT INTO Annos (nimi) VALUES ('Huono');");
+        lista.add("INSERT INTO Annos (nimi) VALUES ('Hyoky');");*/
 
         return lista;
     }
